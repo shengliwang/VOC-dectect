7b9cd2413da56c6979a6e983859abfee0a5db319
vicking <vicking@mail.ustc.edu.cn>
2022-11-01 17:14:32 +0800

    解决 合上标签就不能显示的问题

--- web/js/main.js
+++ web/js/main.js
@@ -9,41 +9,11 @@ https://docs.webix.com/api__link__ui.tree_ondblclick_config.html
 // 初始化画布
 let myChart = null;
 
-const baseUrl = "./data"
-
-
 // 初始化导航树
+const baseUrl = "./data";
 let tree = null;
-// webix.ui({
-//   container:"tree",
-//   view:"tree",
-//   select:true,
-//   data: [
-//     {id:"root", value:"Cars", open:true, data:[
-//       { id:"1", open:true, value:"Toyota", data:[
-//         { id:"1.1", value:"Avalon" },
-//         { id:"1.2", value:"Corolla" },
-//         { id:"1.3", value:"Camry" }
-//       ]},
-//       { id:"2", value:"Skoda", open:true, data:[
-//         { id:"2.1", value:"Octavia" },
-//         { id:"2.2", value:"Superb" }
-//       ]},
-//       { id:"2", value:"Skoda"
-//       }
-//     ]}
-//   ]
-// });
 
-tree = webix.ui({
-  container:"tree",
-  view:"tree",
-  type:"lineTree",
-  data: [
-      {id: "root", value: "root", open: true}
-  ]
-});
-
+/*更新图表的函数*/
 function updateChart(data){
   const chartdata ={
       labels: data.dataX,
@@ -77,77 +47,104 @@ function updateChart(data){
   myChart = new Chart(document.getElementById("chart"), config);
 }
 
-
-tree.attachEvent("onItemDblClick", function(id) {
-  url = tree.getItemNode(id).getAttribute("myUrl");
-
-  console.log("myUrl is: " + url);
-
+/*
+* 创建树的递归函数 
+*/
+function treeAddElementRecsive(base_url, treeParentId)
+{
+  const url = base_url+ "/config.json";
   const httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE
-        && this.status == 200){
-      const data = JSON.parse(this.responseText);
-      updateChart(data)
+      && this.status == 200)
+    {
+      treeAddNode(treeParentId, base_url, JSON.parse(this.responseText));
     }
-  };
-  httpRequest.open("GET", url, true);
-  httpRequest.send();  
-});
-
-initTree(baseUrl);
-
-function treeAddElementRecsive(baseUrl, treeParentId)
-{
-  const url = baseUrl+ "/config.json";
-  const httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function() {
-    httpRequestCallback(this, treeParentId);
   };
   httpRequest.open("GET", url, true);
   httpRequest.send();
 }
 
-function httpRequestCallback(handle, treeParentId){
-  if (handle.readyState == XMLHttpRequest.DONE){
-      if (handle.status == 200){
-          data = JSON.parse(handle.responseText);
-          const id = tree.add(
-            {id:"", value: "显示" + data.dirUint + "图表", open: false,
-              $css: "filestyle"},
-            0, treeParentId
-          );
+let id_counter = 0;
+function treeAddNode(treeParentId, base_url, data){
+  id_counter ++;
+  tree.add(
+      {id:"idhash_" + String(id_counter), value: "显示" + data.dirUint + "图表", 
+      open: false, $css: "filestyle"},
+       0, treeParentId
+  );
 
-          const ele = tree.getItemNode(id);
-          ele.setAttribute("myUrl", handle.responseURL);
+  for (i in data.childDirs){
+    tree.add(
+      {id: data.childDirs[i], value: data.childDirs[i]+data.childDirsUnit, 
+        open:true, $css: "dirstyle"},
+      -1, treeParentId
+    );
 
-          for (i in data.childDirs){
-            console.log("i=" + i);
-            tree.add(
-              {id: data.childDirs[i], value: data.childDirs[i]+data.childDirsUnit, 
-                open:true, $css: "dirstyle"},
-              -1, treeParentId
-            );
-            const base = (new URL(".", handle.responseURL)).toString();
-            const newurl = base + data.childDirs[i];
-            treeAddElementRecsive(newurl, data.childDirs[i]);
-          }
-      } else {
-          console.log("error to get url: " + (handle.responseURL) );
-      }
+    const newurl = base_url + "/" + data.childDirs[i];
+    treeAddElementRecsive(newurl, data.childDirs[i]);
   }
 }
 
-
+/* 初始化树的函数 */
 function initTree(base_url){
+    tree = webix.ui({
+      container:"tree",
+      view:"tree",
+      type:"lineTree",
+      data: [
+          {id: "root", value: "root", open: true}
+      ]
+    });
+
     const url = base_url+ "/config.json"
     const httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
-      httpRequestCallback(this, "root");
+      if (this.readyState == XMLHttpRequest.DONE
+        && this.status == 200)
+      {
+        treeAddNode("root", base_url, JSON.parse(this.responseText));
+      } 
     };
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
 
+/* 调用函数初始化树 */
+initTree(baseUrl);
 
+/* onItemDblClick */
+/*
+ * 注册文件树的回调函数（单击，双击的话，要注册上面的事件）
+*/
+tree.attachEvent("onItemClick", function(id) {
+  if (id.match("^idhash_.*") == null)
+  {
+    return ;
+  }
+
+  let parentId = null;
+  parentId = tree.getParentId(id);
+ 
+  let url = "/config.json";
+  while(parentId != "root")
+  {
+    url = "/" + parentId + url;
+    parentId = tree.getParentId(parentId)
+  }
+
+  url = baseUrl + url;
+
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE
+        && this.status == 200){
+      const data = JSON.parse(this.responseText);
+      /* 更新图表 */
+      updateChart(data)
+    }
+  };
+  httpRequest.open("GET", url, true);
+  httpRequest.send();  
+});
